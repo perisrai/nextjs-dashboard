f51cc1fd5ad8a8c8d9ac09e4d111cc6574b0a2e7
Peris Rai <peris@Periss-MacBook-Pro.local>
2024-05-13 15:04:04 +0545

    chapter12: use server action to implement create, edit and delete invoice actions

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,8 +10,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`);
-
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
 
@@ -23,10 +21,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
     replace(`${pathname}?${params.toString()}`);
   }, 500);
-
-  // function handleSearch(term: string) {
-
-  // }
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
